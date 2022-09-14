e1e5249e3145e8f4af8c5d624ad2b395b13ccd4f
Tulasi Joshua Mbawini <tulasijoshuambawini@gmail.com>
2022-09-13 17:15:35 -0700

    square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-# 0-square.py by Tulasi Joshua
-"""A module that defines a square """
+# 2-square.py
+# Tulasi Joshua Mbawini
+"""Define a class Square."""
 
 
 class Square:
-    """A class that represents a square"""
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """Initializing this square class
+        """Initialize a new Square.
         Args:
-            size: represnets the size of the square defined
-        Raises:
-            TypeError: if size is not integer
-            ValueError: if size is less than zero
+            size (int): The size of the new square.
         """
-
         if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
-        self.__size = sizei
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
